13c940d577681bf54d6c9de77c6abb324109b2d2
evan.fang <evan.fang@linecorp.com>
2019-06-26 15:38:33 +0800

    Init commit

--- src/main/java/com/odde/securetoken/ProfileDao.java
+++
@@ -1,7 +0,0 @@
-package com.odde.securetoken;
-
-public class ProfileDao {
-    public String getPassword(String account) {
-        return Context.getPassword(account);
-    }
-}
---
+++ src/test/java/com/odde/securetoken/QueryBudgetTest.java
@@ -0,0 +1,5 @@
+package com.odde.securetoken;
+
+public class QueryBudgetTest {
+
+}
